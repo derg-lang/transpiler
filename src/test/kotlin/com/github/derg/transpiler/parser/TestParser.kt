3dd5c84b5d193a65ca77a2d71fad52a6242f904d
Argentail Glacies <35341006+ArgentailGlacies@users.noreply.github.com>
2022-11-04 21:35:33 +0100

    Add initial ast parser

--- src/test/kotlin/com/github/derg/transpiler/parser/TestParser.kt
+++ src/test/kotlin/com/github/derg/transpiler/parser/TestParser.kt
@@ -1,5 +1,25 @@
 package com.github.derg.transpiler.parser
 
+import com.github.derg.transpiler.ast.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
 class TestParser
 {
+    @Test
+    fun `Given empty segment, when parsing, then correctly parsed`()
+    {
+        val expected = Segment(module = null, imports = emptySet(), definitions = emptyList())
+        
+        assertEquals(expected, parse(""))
+    }
+    
+    @Test
+    fun `Given populated segment, when parsing, then correctly parsed`()
+    {
+        val variable = Variable("foo", null, Value.Real(42.toBigDecimal(), "i8"), Visibility.PRIVATE, Mutability.VALUE)
+        val expected = Segment(module = null, imports = emptySet(), definitions = listOf(variable))
+        
+        assertEquals(expected, parse("val foo = 42i8"))
+    }
 }
