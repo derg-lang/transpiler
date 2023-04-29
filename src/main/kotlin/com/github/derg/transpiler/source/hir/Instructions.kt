5cedbb0ab4188cda5f0ab89fd9b620cd8b7665c6
Argentail Glacies <35341006+ArgentailGlacies@users.noreply.github.com>
2023-04-29 17:25:40 +0200

    Add initial instructions and values

--- src/main/kotlin/com/github/derg/transpiler/source/hir/Instructions.kt
+++ src/main/kotlin/com/github/derg/transpiler/source/hir/Instructions.kt
@@ -5,3 +5,42 @@ package com.github.derg.transpiler.source.hir
  * although it may be composed out of multiple calculations.
  */
 sealed interface Instruction
+
+/**
+ * Assigns the specified [value] to the [variable].
+ */
+data class Assign(val variable: Variable, val value: Value) : Instruction
+
+/**
+ * Conditional execution is possible by branching the control flow one a [predicate]. If the predicates matches, the
+ * [success] branch is selected, otherwise [failure] is.
+ */
+data class Condition(val predicate: Value, val success: Branch, val failure: Branch) : Instruction
+{
+    class Branch(val symbols: SymbolTable, val instructions: List<Instruction>)
+}
+
+/**
+ * Exits the current function call, returning control flow to whomever called the function in the first place. No
+ * return value is provided by the function.
+ */
+object Exit : Instruction
+
+/**
+ * The null operation, does nothing. It will always be removed from the instruction set. It is used to handle various
+ * special cases where statements contributing to the structure of the program occur. Such statements do not include any
+ * executable code, and thus will be omitted.
+ */
+object Nop : Instruction
+
+/**
+ * Exits the current function call, returning control flow to whoever called the function in the first place. The
+ * return value of the function is provided by the given [value].
+ */
+data class Return(val value: Value) : Instruction
+
+/**
+ * Exits the current function call, returning control flow to whoever called the function in the first place. The
+ * error value of the function is provided by the given [error].
+ */
+data class Raise(val error: Value) : Instruction
