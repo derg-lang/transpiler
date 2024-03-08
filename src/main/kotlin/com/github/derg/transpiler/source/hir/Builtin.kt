9cbd4c85852ee79495ca219f9b6c5c31e4e393ee
Argentail Glacies <35341006+ArgentailGlacies@users.noreply.github.com>
2024-03-08 18:01:29 +0100

    Replace operator name with operator symbol

--- src/main/kotlin/com/github/derg/transpiler/source/hir/Builtin.kt
+++ src/main/kotlin/com/github/derg/transpiler/source/hir/Builtin.kt
@@ -25,44 +25,44 @@ object Builtin
     val VOID = registerType(VOID_TYPE_NAME)
     
     val BOOL = registerType(BOOL_TYPE_NAME)
-    val BOOL_AND = registerInfixOp(Symbol.AND.symbol, BOOL, BOOL, null)
-    val BOOL_EQ = registerInfixOp(Symbol.EQUAL.symbol, BOOL, BOOL, null)
-    val BOOL_NE = registerInfixOp(Symbol.NOT_EQUAL.symbol, BOOL, BOOL, null)
-    val BOOL_NOT = registerPrefixOp(Symbol.NOT.symbol, BOOL, BOOL, null)
-    val BOOL_OR = registerInfixOp(Symbol.OR.symbol, BOOL, BOOL, null)
-    val BOOL_XOR = registerInfixOp(Symbol.XOR.symbol, BOOL, BOOL, null)
+    val BOOL_AND = registerInfixOp(Symbol.AND, BOOL, BOOL, null)
+    val BOOL_EQ = registerInfixOp(Symbol.EQUAL, BOOL, BOOL, null)
+    val BOOL_NE = registerInfixOp(Symbol.NOT_EQUAL, BOOL, BOOL, null)
+    val BOOL_NOT = registerPrefixOp(Symbol.NOT, BOOL, BOOL, null)
+    val BOOL_OR = registerInfixOp(Symbol.OR, BOOL, BOOL, null)
+    val BOOL_XOR = registerInfixOp(Symbol.XOR, BOOL, BOOL, null)
     
     val INT32 = registerType(INT32_TYPE_NAME)
     val INT32_LIT = registerLiteral(INT32_LIT_NAME, INT32)
-    val INT32_EQ = registerInfixOp(Symbol.EQUAL.symbol, INT32, BOOL, null)
-    val INT32_GE = registerInfixOp(Symbol.GREATER_EQUAL.symbol, INT32, BOOL, null)
-    val INT32_GT = registerInfixOp(Symbol.GREATER.symbol, INT32, BOOL, null)
-    val INT32_LE = registerInfixOp(Symbol.LESS_EQUAL.symbol, INT32, BOOL, null)
-    val INT32_LT = registerInfixOp(Symbol.LESS.symbol, INT32, BOOL, null)
-    val INT32_NE = registerInfixOp(Symbol.NOT_EQUAL.symbol, INT32, BOOL, null)
-    val INT32_ADD = registerInfixOp(Symbol.PLUS.symbol, INT32, INT32, VOID)
-    val INT32_DIV = registerInfixOp(Symbol.DIVIDE.symbol, INT32, INT32, VOID)
-    val INT32_MOD = registerInfixOp(Symbol.MODULO.symbol, INT32, INT32, VOID)
-    val INT32_MUL = registerInfixOp(Symbol.MULTIPLY.symbol, INT32, INT32, VOID)
-    val INT32_NEG = registerPrefixOp(Symbol.MINUS.symbol, INT32, INT32, VOID)
-    val INT32_POS = registerPrefixOp(Symbol.PLUS.symbol, INT32, INT32, VOID)
-    val INT32_SUB = registerInfixOp(Symbol.MINUS.symbol, INT32, INT32, VOID)
+    val INT32_EQ = registerInfixOp(Symbol.EQUAL, INT32, BOOL, null)
+    val INT32_GE = registerInfixOp(Symbol.GREATER_EQUAL, INT32, BOOL, null)
+    val INT32_GT = registerInfixOp(Symbol.GREATER, INT32, BOOL, null)
+    val INT32_LE = registerInfixOp(Symbol.LESS_EQUAL, INT32, BOOL, null)
+    val INT32_LT = registerInfixOp(Symbol.LESS, INT32, BOOL, null)
+    val INT32_NE = registerInfixOp(Symbol.NOT_EQUAL, INT32, BOOL, null)
+    val INT32_ADD = registerInfixOp(Symbol.PLUS, INT32, INT32, VOID)
+    val INT32_DIV = registerInfixOp(Symbol.DIVIDE, INT32, INT32, VOID)
+    val INT32_MOD = registerInfixOp(Symbol.MODULO, INT32, INT32, VOID)
+    val INT32_MUL = registerInfixOp(Symbol.MULTIPLY, INT32, INT32, VOID)
+    val INT32_NEG = registerPrefixOp(Symbol.MINUS, INT32, INT32, VOID)
+    val INT32_POS = registerPrefixOp(Symbol.PLUS, INT32, INT32, VOID)
+    val INT32_SUB = registerInfixOp(Symbol.MINUS, INT32, INT32, VOID)
     
     val INT64 = registerType(INT64_TYPE_NAME)
     val INT64_LIT = registerLiteral(INT64_LIT_NAME, INT64)
-    val INT64_EQ = registerInfixOp(Symbol.EQUAL.symbol, INT64, BOOL, null)
-    val INT64_GE = registerInfixOp(Symbol.GREATER_EQUAL.symbol, INT64, BOOL, null)
-    val INT64_GT = registerInfixOp(Symbol.GREATER.symbol, INT64, BOOL, null)
-    val INT64_LE = registerInfixOp(Symbol.LESS_EQUAL.symbol, INT64, BOOL, null)
-    val INT64_LT = registerInfixOp(Symbol.LESS.symbol, INT64, BOOL, null)
-    val INT64_NE = registerInfixOp(Symbol.NOT_EQUAL.symbol, INT64, BOOL, null)
-    val INT64_ADD = registerInfixOp(Symbol.PLUS.symbol, INT64, INT64, VOID)
-    val INT64_DIV = registerInfixOp(Symbol.DIVIDE.symbol, INT64, INT64, VOID)
-    val INT64_MOD = registerInfixOp(Symbol.MODULO.symbol, INT64, INT64, VOID)
-    val INT64_MUL = registerInfixOp(Symbol.MULTIPLY.symbol, INT64, INT64, VOID)
-    val INT64_NEG = registerPrefixOp(Symbol.MINUS.symbol, INT64, INT64, VOID)
-    val INT64_POS = registerPrefixOp(Symbol.PLUS.symbol, INT64, INT64, VOID)
-    val INT64_SUB = registerInfixOp(Symbol.MINUS.symbol, INT64, INT64, VOID)
+    val INT64_EQ = registerInfixOp(Symbol.EQUAL, INT64, BOOL, null)
+    val INT64_GE = registerInfixOp(Symbol.GREATER_EQUAL, INT64, BOOL, null)
+    val INT64_GT = registerInfixOp(Symbol.GREATER, INT64, BOOL, null)
+    val INT64_LE = registerInfixOp(Symbol.LESS_EQUAL, INT64, BOOL, null)
+    val INT64_LT = registerInfixOp(Symbol.LESS, INT64, BOOL, null)
+    val INT64_NE = registerInfixOp(Symbol.NOT_EQUAL, INT64, BOOL, null)
+    val INT64_ADD = registerInfixOp(Symbol.PLUS, INT64, INT64, VOID)
+    val INT64_DIV = registerInfixOp(Symbol.DIVIDE, INT64, INT64, VOID)
+    val INT64_MOD = registerInfixOp(Symbol.MODULO, INT64, INT64, VOID)
+    val INT64_MUL = registerInfixOp(Symbol.MULTIPLY, INT64, INT64, VOID)
+    val INT64_NEG = registerPrefixOp(Symbol.MINUS, INT64, INT64, VOID)
+    val INT64_POS = registerPrefixOp(Symbol.PLUS, INT64, INT64, VOID)
+    val INT64_SUB = registerInfixOp(Symbol.MINUS, INT64, INT64, VOID)
     
     // TODO: Support strings somehow
     val STR = registerType(STR_TYPE_NAME)
@@ -95,12 +95,12 @@ private fun registerLiteral(name: String, type: HirStruct) = HirLiteral(
 ).also(Builtin.GLOBAL_SCOPE::register)
 
 /**
- * Defines a new infix operator for the given [name]. The [parameter] type will be the same for both the left- and the
- * right-hand side expressions. The operator returns a value of the given [value] and [error] types.
+ * Defines a new infix operator for the given [operator]. The [parameter] type will be the same for both the left- and
+ * the right-hand side expressions. The operator returns a value of the given [value] and [error] types.
  */
-private fun registerInfixOp(name: String, parameter: HirStruct, value: HirStruct, error: HirStruct?) = HirFunction(
+private fun registerInfixOp(operator: Symbol, parameter: HirStruct, value: HirStruct, error: HirStruct?) = HirFunction(
     id = UUID.randomUUID(),
-    name = name,
+    name = operator.symbol,
     visibility = Visibility.EXPORTED,
     value = HirTypeStruct(value.name, emptyList(), Mutability.IMMUTABLE),
     error = error?.let { HirTypeStruct(it.name, emptyList(), Mutability.IMMUTABLE) },
@@ -111,12 +111,12 @@ private fun registerInfixOp(name: String, parameter: HirStruct, value: HirStruct
 ).also(Builtin.GLOBAL_SCOPE::register)
 
 /**
- * Defines a new prefix operator for the given [name]. The [parameter] type determines which expressions are legal. The
- * operator returns a value of the given [value] and [error] types.
+ * Defines a new prefix operator for the given [operator]. The [parameter] type determines which expressions are legal.
+ * The operator returns a value of the given [value] and [error] types.
  */
-private fun registerPrefixOp(name: String, parameter: HirStruct, value: HirStruct, error: HirStruct?) = HirFunction(
+private fun registerPrefixOp(operator: Symbol, parameter: HirStruct, value: HirStruct, error: HirStruct?) = HirFunction(
     id = UUID.randomUUID(),
-    name = name,
+    name = operator.symbol,
     visibility = Visibility.EXPORTED,
     value = HirTypeStruct(value.name, emptyList(), Mutability.IMMUTABLE),
     error = error?.let { HirTypeStruct(it.name, emptyList(), Mutability.IMMUTABLE) },
